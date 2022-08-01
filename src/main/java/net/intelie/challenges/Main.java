46a3697b4c5b8f832515c1888f826922d5b6ab66
Adriana Osorio <driosorio@gmail.com>
2022-08-01 19:34:25 -0300

    Create Main.java

---
+++ src/main/java/net/intelie/challenges/Main.java
@@ -0,0 +1,16 @@
+public class Main implements EventStore {
+  
+  public static void main(String[] args) {
+    
+  }
+
+  void insert(Event event) {
+     
+  }
+  
+  void removeAll(String type) {
+  }
+  
+  EventIterator query(String type, long startTime, long endTime);
+  
+}
